e154ea05f3857cb7f84892da08fce82a8f53192e
zivkaziv <zivkalderon95@gmail.com>
2024-04-29 17:57:03 +0300

    add npx support

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const fs = require("fs");
-fs.readFile(
-	"./info.json",
-	{encoding:'utf-8'},
-	((err, data) => {
-		if(err) console.log(err)
-        console.log(data)
-	})
-);
+(() => {
+	const fs = require("fs");
+	fs.readFile("./info.json", { encoding: "utf-8" }, (err, data) => {
+		if (err) console.log(err);
+		console.log(data);
+	});
+})();
