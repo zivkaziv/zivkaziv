72bae998767096e93873064cb3d18d3b4815a74b
zivkaziv <zivkalderon95@gmail.com>
2024-06-02 07:34:29 +0300

    simplify

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env node
 (() => {
-	const fs = require("fs");
-	fs.readFile("./info.json", { encoding: "utf-8" }, (err, data) => {
-		if (err) console.log(err);
-		console.log(data);
-	});
+	console.log({
+		"name": "zivkaziv",
+		"version": "0.0.6",
+		"description": "Ziv Kalderon contact info",
+		"main": "index.js",
+		"repository": "zivkaziv/zivkaziv",
+		"scripts": {
+			"postinstall": "cat info.json",
+			"start": "node ."
+		},
+		"bin": "./index.js",
+		"keywords": [
+			"person",
+			"contact",
+			"zivkaziv",
+			"ziv kalderon"
+		],
+		"author": "Ziv Kalderon <kalziv@gmail.com>",
+		"license": "GPL-3.0",
+		"dependencies": {
+			"zivkaziv": "^0.0.2"
+		}
+	})
 })();
